3ce75084b0d67bb900c9c8af7401a81b9045f790
SupaPay <supaapay@gmail.com>
2025-09-03 17:17:13 +0100

    protection

--- routes/admin.js
+++ routes/admin.js
@@ -7,26 +7,27 @@ const {
   sendEmailToUser,
   fundUserAccount
 } = require('../controllers/adminController');
-const { protect } = require('../middleware/auth');
+const adminAuth = require('../middleware/adminAuth');
+
 
 const router = express.Router();
 
 // Protect all routes, only admins
-router.use(protect); 
+router.use(adminAuth); 
 
 // Get all users
-router.get('/users', getAllUsers);
+router.get('/users', adminAuth, getAllUsers);
 
 // Create a new user
-router.post('/create-user', createUser);
+router.post('/create-user', adminAuth, createUser);
 
 // Delete a user
-router.delete('/delete-user/:id', deleteUser);
+router.delete('/delete-user/:id', adminAuth, deleteUser);
 
 // Send email to a user
-router.post('/send-email', sendEmailToUser);
+router.post('/send-email', adminAuth, sendEmailToUser);
 
 // Fund user account
-router.post('/fund-user', fundUserAccount);
+router.post('/fund-user', adminAuth, fundUserAccount);
 
 module.exports = router;
