f3935005d896ae173446f081d00edc960c2b0a29
SupaPay <supaapay@gmail.com>
2025-09-14 06:19:11 +0100

    rout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,34 +1,4 @@
-// // routes/auth.js
-// const express = require("express");
-// const {
-//   register,
-//   login,
-//   forgotPassword,
-//   resetPassword,
-//   registerAdmin,
-//   loginAdmin, 
-//   forgotAdminPassword, 
-//   resetAdminPassword,
-// } = require("../controllers/authController");
-// const { logout } = require("../controllers/userController");
 
-// const router = express.Router();
-
-// // Public routes
-// router.post("/register", register);
-// router.post("/login", login);
-// router.post("/forgot-password", forgotPassword);
-// router.put("/reset-password/:token", resetPassword);
-// router.post("/admin/register", registerAdmin);
-// router.post("/admin/login", loginAdmin);
-// router.post("/admin/forgot-password", forgotAdminPassword);
-// router.put("/admin/reset-password/:token", resetAdminPassword);
-// router.post('/api/auth/logout', logout);
-
-// module.exports = router;
-
-
-// routes/authRoutes.js
 // routes/authRoutes.js
 const express = require("express");
 const {
@@ -43,7 +13,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
 
